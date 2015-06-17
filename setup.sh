88d981aa506f80386f22830131f2b8cf3ced76eb
Jia-Shen Boon <jiashen@gmail.com>
2015-06-17 11:34:03 -0500

    Add copy

--- setup.sh
+++ setup.sh
@@ -4,7 +4,12 @@
 #>> git clone https://github.com/boonjiashen/.vim
 #>> cd .vim
 #>> chmod +x setup.sh
+
+# Install Pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 git submodule init
 git submodule update
+
+cp -i .vimrc ~/.vimrc # -i: prompts if you wanna overwrite existing file
